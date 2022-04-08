46ef220aec0b86e7db7dd3748807a6fe73b4ba87
Raj Viradiya <72543111+Rajviradiyacoder4024@users.noreply.github.com>
2022-04-08 16:50:55 +0530

    Update 19it152.py

--- 19it152.py
+++ 19it152.py
@@ -1,1 +1,27 @@
-print ('Hello Python') 
+pipeline {
+    agent any
+    stages {
+        stage('git repo & clean') {
+            steps {
+               bat "rmdir  /s /q Inventory_management_system"
+                bat "git clone https://github.com/Rajviradiyacoder4024/19it152.git"
+                bat "mvn clean -f Inventory_management_system"
+            }
+        }
+        stage('install') {
+            steps {
+                bat "mvn install -f Inventory_management_system"
+            }
+        }
+        stage('test') {
+            steps {
+                bat "mvn test -f Inventory_management_system"
+            }
+        }
+        stage('package') {
+            steps {
+                bat "mvn package -f Inventory_management_system"
+            }
+        }
+    }
+}
